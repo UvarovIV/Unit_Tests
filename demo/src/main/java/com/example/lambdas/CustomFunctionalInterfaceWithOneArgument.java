f0119724b5f852ce6381f530e8e68d35311a5e3f
UvarovIV <post689@mail.ru>
2022-07-10 10:48:39 +0300

    added lambdas package

---
+++ demo/src/main/java/com/example/lambdas/CustomFunctionalInterfaceWithOneArgument.java
@@ -0,0 +1,6 @@
+package com.example.lambdas;
+
+@FunctionalInterface
+public interface CustomFunctionalInterfaceWithOneArgument {
+    int doubleTheNumber(int firstArg);
+}
---
+++ demo/src/main/java/com/example/lambdas/CustomFunctionalInterfaceWithTwoArguments.java
@@ -0,0 +1,6 @@
+package com.example.lambdas;
+
+@FunctionalInterface
+public interface CustomFunctionalInterfaceWithTwoArguments {
+    int raiseToPower(int firstArg, int secondArg);
+}
