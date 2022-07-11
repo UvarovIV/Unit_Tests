0742f58f3e694096d0cfc02b12d6c1625a091722
UvarovIV <post689@mail.ru>
2022-07-12 00:34:59 +0300

    fixes

--- demo/src/test/java/com/example/reflection/WorkWithReflectionTest.java
+++ demo/src/test/java/com/example/reflection/WorkWithReflectionTest.java
@@ -7,38 +7,38 @@ import org.junit.Test;
 
 public class WorkWithReflectionTest {
 
-    final WorkWithReflection wwrf = new WorkWithReflection();
+    final WorkWithReflection workWithReflection = new WorkWithReflection("private", "public", "protected");
+    final Class<?> classForReflection = workWithReflection.getClass();
 
     @Test
     public void workWithFields() throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
-        Field field = wwrf.getClass().getDeclaredField("privateField");
+        Field field = classForReflection.getDeclaredField("privateField");
         field.setAccessible(true);
-        field.set(wwrf, "private");
-        System.out.println(field.get(wwrf));
-        System.out.println(wwrf.getClass().getDeclaredField("publicField").getName());
-        System.out.println(wwrf.getClass().getDeclaredField("protectedField").getType());
+        field.set(workWithReflection, "private");
+        System.out.println(field.get(workWithReflection));
+        System.out.println(classForReflection.getDeclaredField("publicField").getName());
+        System.out.println(classForReflection.getDeclaredField("protectedField").getType());
     }
 
     @Test
-    public void workWithMethods() throws NoSuchFieldException, IllegalArgumentException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
-        Method method = wwrf.getClass().getDeclaredMethod("randomNumber");
+    public void workWithMethods() throws IllegalArgumentException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+        Method method = classForReflection.getDeclaredMethod("randomNumber");
         method.setAccessible(true);
-        System.out.println(method.invoke(wwrf));
-        Arrays.stream(wwrf.getClass().getSuperclass().getDeclaredMethods()).forEach(
-            f -> {
-                System.out.println(f.getName());
-            }
+        System.out.println(method.invoke(workWithReflection));
+        Arrays.stream(classForReflection.getSuperclass().getDeclaredMethods()).forEach(
+            f -> System.out.println(f.getName())
         );
         Arrays.stream(method.getExceptionTypes()).forEach(x -> System.out.println(x.getSimpleName()));
     }
 
     @Test
-    public void workWithConstructors() throws NoSuchFieldException, IllegalArgumentException, IllegalAccessException, NoSuchMethodException, InvocationTargetException, InstantiationException {
+    public void workWithConstructors() throws IllegalArgumentException, IllegalAccessException, NoSuchMethodException, InvocationTargetException, InstantiationException {
         Constructor<WorkWithReflection> publicConstructor = WorkWithReflection.class.getDeclaredConstructor(String.class, String.class, String.class);
         System.out.println(publicConstructor);
         Constructor<WorkWithReflection> privateConstructor = WorkWithReflection.class.getDeclaredConstructor(String.class, String.class);
         privateConstructor.setAccessible(true);
-        WorkWithReflection wwrf2 = privateConstructor.newInstance("fsdzkdks","dnjkuj");
+        WorkWithReflection workWithReflection2 = privateConstructor.newInstance("fsdzkdks","dnjkuj");
+        System.out.println(workWithReflection2.getClass());
         Arrays.stream(publicConstructor.getParameterTypes()).forEach(x -> System.out.println(x.getName()));
     }
 
